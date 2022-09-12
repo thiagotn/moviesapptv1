0afb066acc984c68354a299662d8a171139655cf
Thiago Teixeira Nogueira <tnogueira@uolinc.com>
2022-09-12 20:32:02 -0300

    fix: orientation

--- lib/pages/player/widgets/landscape_player_controls.dart
+++ lib/pages/player/widgets/landscape_player_controls.dart
@@ -1,6 +1,5 @@
 import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:moviesapptv1/pages/player/widgets/play_toggle.dart';
 
 class LandscapePlayerControls extends StatelessWidget {
@@ -53,58 +52,56 @@ class LandscapePlayerControls extends StatelessWidget {
                         width: 10,
                       ),
                       Expanded(
-                        child: Container(
-                          child: FlickVideoProgressBar(
-                            flickProgressBarSettings: FlickProgressBarSettings(
-                              height: 10,
-                              handleRadius: 10,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0,
-                                vertical: 8,
-                              ),
-                              backgroundColor: Colors.white24,
-                              bufferedColor: Colors.white38,
-                              getPlayedPaint: (
-                                  {double? handleRadius,
-                                  double? height,
-                                  double? playedPart,
-                                  double? width}) {
-                                return Paint()
-                                  ..shader = const LinearGradient(colors: [
-                                    Color.fromRGBO(108, 165, 242, 1),
-                                    Color.fromRGBO(97, 104, 236, 1)
-                                  ], stops: [
-                                    0.0,
-                                    0.5
-                                  ]).createShader(
-                                    Rect.fromPoints(
-                                      const Offset(0, 0),
-                                      Offset(width!, 0),
-                                    ),
-                                  );
-                              },
-                              getHandlePaint: (
-                                  {double? handleRadius,
-                                  double? height,
-                                  double? playedPart,
-                                  double? width}) {
-                                return Paint()
-                                  ..shader = const RadialGradient(
-                                    colors: [
-                                      Color.fromRGBO(97, 104, 236, 1),
-                                      Color.fromRGBO(97, 104, 236, 1),
-                                      Colors.white,
-                                    ],
-                                    stops: [0.0, 0.4, 0.5],
-                                    radius: 0.4,
-                                  ).createShader(
-                                    Rect.fromCircle(
-                                      center: Offset(playedPart!, height! / 2),
-                                      radius: handleRadius!,
-                                    ),
-                                  );
-                              },
+                        child: FlickVideoProgressBar(
+                          flickProgressBarSettings: FlickProgressBarSettings(
+                            height: 10,
+                            handleRadius: 10,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0,
+                              vertical: 8,
                             ),
+                            backgroundColor: Colors.white24,
+                            bufferedColor: Colors.white38,
+                            getPlayedPaint: (
+                                {double? handleRadius,
+                                double? height,
+                                double? playedPart,
+                                double? width}) {
+                              return Paint()
+                                ..shader = const LinearGradient(colors: [
+                                  Color.fromRGBO(108, 165, 242, 1),
+                                  Color.fromRGBO(97, 104, 236, 1)
+                                ], stops: [
+                                  0.0,
+                                  0.5
+                                ]).createShader(
+                                  Rect.fromPoints(
+                                    const Offset(0, 0),
+                                    Offset(width!, 0),
+                                  ),
+                                );
+                            },
+                            getHandlePaint: (
+                                {double? handleRadius,
+                                double? height,
+                                double? playedPart,
+                                double? width}) {
+                              return Paint()
+                                ..shader = const RadialGradient(
+                                  colors: [
+                                    Color.fromRGBO(97, 104, 236, 1),
+                                    Color.fromRGBO(97, 104, 236, 1),
+                                    Colors.white,
+                                  ],
+                                  stops: [0.0, 0.4, 0.5],
+                                  radius: 0.4,
+                                ).createShader(
+                                  Rect.fromCircle(
+                                    center: Offset(playedPart!, height! / 2),
+                                    radius: handleRadius!,
+                                  ),
+                                );
+                            },
                           ),
                         ),
                       ),
@@ -129,10 +126,6 @@ class LandscapePlayerControls extends StatelessWidget {
           top: 10,
           child: GestureDetector(
             onTap: () {
-              SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-                  overlays: SystemUiOverlay.values);
-              SystemChrome.setPreferredOrientations(
-                  [DeviceOrientation.portraitUp]);
               Navigator.pop(context);
             },
             child: const Icon(
