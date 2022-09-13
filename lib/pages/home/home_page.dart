87f585a01a305881b20385ffe4b235091b01ab87
Thiago Teixeira Nogueira <tnogueira@uolinc.com>
2022-09-13 18:05:02 -0300

    fix: initial path

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:moviesapptv1/pages/widgets/carousel_widget.dart';
 
 class HomePage extends StatelessWidget {
-  static var routeName = "/home";
+  static var routeName = "/";
 
   const HomePage({Key? key}) : super(key: key);
 
